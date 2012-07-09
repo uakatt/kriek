7a45539c80b36f27340f4cf3f31158b699a60f1b
Sam Rawlins <sam.rawlins@gmail.com>
2012-07-09 14:43:30 -0700

    Changed KITTs to KATTSs

--- kriek.rb
+++ kriek.rb
@@ -1,6 +1,7 @@
-# WHAT the WHAAAT: cherry pick script
+# cherry pick script
 #
-# GOAL:            * interactively add a rebase, and individual commits that the user intends to fold in.
+# GOAL: * interactively add a rebase, and individual commits that the user intends to fold in.
+
 require_relative './lib/kriek'
 require 'readline'
 
@@ -16,11 +17,11 @@ loop do
     case #inp
     when inp =~ /^\s*SET +DEBUG +(ON|OFF)\s*$/i                         # SET DEBUG
       k.debug = ($1 =~ /on/i ? true : false)
-    when inp =~ /^\s*SET +KITT +((kitt-)?\d+)\s*$/i                     # SET KITT
+    when inp =~ /^\s*SET +KATTS +((katts-)?\d+)\s*$/i                     # SET KITT
       kitt = $1
-      kitt = kitt =~ /kitt-(\d+)/i ? $1 : kitt
-      print "Setting KITT to KITT-#{kitt}..."
-      k.kitt = "KITT-#{kitt}"
+      kitt = kitt =~ /katts-(\d+)/i ? $1 : kitt
+      print "Setting KATTS to KATTS-#{kitt}..."
+      k.kitt = "KATTS-#{kitt}"
       puts " done."
 
 
@@ -31,7 +32,7 @@ loop do
       k.release_number = "3.0-#{rel}"
       puts " done."
 
-    when inp =~ /^\s*(MENU|H(el?|elp)?|\?)\s*$/i                        # MENU | Quit | ?
+    when inp =~ /^\s*(MENU|H(el?|elp)?|\?)\s*$/i                        # MENU | Help | ?
       puts k.menu
 
     when inp =~ /^\s*A(dd?)? +R(an?|ange?)? +(\d+:\d+)\s*$/i            # Add Range
@@ -49,67 +50,67 @@ loop do
           puts result ? " done." : "\nError: #{c} is already in the merge list."
         end
       end
-    
+
     when inp =~ /^\s*A(dd?)? +LIQ(ui?|uiba?|uibase?)? +([0-9 ]+)\s*$/i  # Add LIQuibase
       rev = $3
       print "Adding liquibase changeset from #{rev}..."
       result = k.add_liquibase rev
       print result
       puts result =~ /Error/i ? "" : " done." 
-    
+
     when inp =~ /^\s*REM(ov?|ove)? +R(an?|ange?)? +(\d+:\d+)\s*$/i      # REMove Range
       print "Removing range: #{$3}..."
       result = k.remove_range $3
       puts result ? " removed." : "\nError: #{$3} was not found!"
-  
+
     when inp =~ /^\s*REM(ov?|ove)? +C(om?|ommi?|ommit)? +(\d+)\s*$/i    # REMove Commit
       print "Removing commit: #{$3}..."
       result = k.remove_commit $3
       puts result ? " removed." : "\nError: #{$3} was not found!"
-  
+
     when inp =~ /^\s*SVN +I(nf?|nfo)?\s*$/i                             # SVN Info
       puts "Printing svn info..."
       puts k.svn_info
-  
+
     when inp =~ /^\s*SVN +L(og?)?( +-v)?\s*$/i                          # SVN Log
       puts "Printing svn log..."
       puts k.svn_log $2
-    
+
     when inp =~ /^\s*FIND +FILEs?\s*$/i                                 # FIND FILEs
       puts "Finding associated files..."
       puts k.files.sort_by {|e| e[:fname]}.map { |e| e[:fname] }
-    
+
     when inp =~ /^\s*FIND +JIRAs?\s*$/i                                 # FIND JIRAs
       puts "Finding associated JIRAs..."
       puts k.jiras.inspect
-    
+
     when inp =~ /^\s*FIND +LIQ(ui?|uiba?|uibase?)?\s*$/i                # FIND LIQuibase
       print "Finding liquibase changesets..."
       liquibase_changesets = k.liquibase_changesets
       puts liquibase_changesets.empty? ? " none." : ""
       liquibase_changesets.each { |l| puts "  "+l }
-    
+
     when inp =~ /^\s*FIND +WORKF(lo?|low)?\s*$/i                        # FIND WORKFlow
       print "Finding workflow changes..."
       workflow_changes = k.workflow_changes
       puts workflow_changes.empty? ? " none." : ""
       workflow_changes.each { |r,workflows| workflows.each { |w| puts "  rev #{r}: #{w}" } }
-  
+
     when inp =~ /^\s*PREV(ie?|iew)?\s*$/i                               # PREView
       puts "Previewing svn commands..."
       puts k.preview
-  
+
     when inp =~ /^\s*RUN\s*$/i                                          # RUN
       puts "Running svn commands..."
       puts k.run!
-  
+
     when inp =~ /^\s*Q(ui?|uit)?\s*$/i                                  # Quit
       puts "Quitting..."
       exit
-  
+
     else
       puts "I did not understand that command. Maybe too many krieks..."
-  
+
     end
   end
 end
